4f37a64ee31d641331f9656716451545b2c0d699
laeciol7 <laeciosousa07@outlook.com>
2022-07-05 20:32:07 -0300

    Primeiro commit projeto whatswebhook

---
+++ whatswebhook/src/main/kotlin/com/br/thelovecats/whatswebhook/WhatswebhookApplication.kt
@@ -0,0 +1,11 @@
+package com.br.thelovecats.whatswebhook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WhatswebhookApplication
+
+fun main(args: Array<String>) {
+	runApplication<WhatswebhookApplication>(*args)
+}
